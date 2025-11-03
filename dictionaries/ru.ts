3fa1f84674f68e31783072b6dc0fa0549fe21d65
taghiyevs4-lgtm <taghiyevs4@gmail.com>
2025-11-03 11:36:11 +0400

    Create ru.ts

---
+++ dictionaries/ru.ts
@@ -0,0 +1,8 @@
+// dictionaries/ru.ts
+export default {
+  brand: "Prime Legal & Consulting",
+  tagline: "Надежный партнер в юридическом и бизнес-росте",
+  nav: { home: "Главная", about: "О нас", services: "Услуги", team: "Команда", contact: "Контакты" },
+  cta: { getInTouch: "Связаться", ourServices: "Наши услуги" },
+  footer: { quickLinks: "Быстрые ссылки", contact: "Контакты" },
+};
